2d81028009c5a8462a5d08fc4559a00e0d748798
ersurajkumar2023 <er.surajkumar2023@gmail.com>
2023-08-09 21:12:33 +0530

    some changes home page

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-      <div>
-        <Navbar />
-           <div className='flex w-full fixed  h-full justify-between overflow-x-hidden overflow-y-hidden'>
-          <div className='md:w-[20%] '><Sidebar  /></div>
+    <div>
+      {/* <Navbar />
+      <div className='flex w-full fixed  h-full justify-between overflow-x-hidden overflow-y-hidden'>
+        <div className='md:w-[20%] '><Sidebar /></div>
         <div className='w-full md:w-[55%]'><Feed /></div>
         <div className='w-[25%]  hidden md:block'><RightSideBar /></div>
-      </div>
+      </div> */}
+      <h1>Hello</h1>
     </div>
   )
 }
