907ddaf792de6822b34222bd125c82c94bd3086d
ersurajkumar2023 <er.surajkumar2023@gmail.com>
2023-08-09 21:21:59 +0530

    Some changes npm install gh-pages --save-dev

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ import Navbar from './components/Navbar'
 const App = () => {
   return (
     <div>
-      {/* <Navbar />
+      <Navbar />
       <div className='flex w-full fixed  h-full justify-between overflow-x-hidden overflow-y-hidden'>
         <div className='md:w-[20%] '><Sidebar /></div>
         <div className='w-full md:w-[55%]'><Feed /></div>
         <div className='w-[25%]  hidden md:block'><RightSideBar /></div>
-      </div> */}
-      <h1>Hello</h1>
+      </div>
     </div>
   )
 }
